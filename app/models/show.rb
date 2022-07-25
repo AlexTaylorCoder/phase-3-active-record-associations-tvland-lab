2573cc3f154dbccd7852a51ae183339798a78228
alextaylor <alextaylor515@gmail.com>
2022-07-25 16:02:18 -0400

    commit tree bffa34aaac2f55f1b25d277c63674d408c6e1a66

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+
+  belongs_to :network
+
+  def actors_list
+    narr = []
+    self.actors.all.each do |act|
+        narr << act.full_name
+        end
+        narr
+    end
 end
